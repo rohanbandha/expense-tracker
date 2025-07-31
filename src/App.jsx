1563aacc51ba9169a11a853787505b2ad7156b59
rohanbandha <rohansibuna@gmail.com>
2025-07-31 23:52:46 +0530

    Repo coonected

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  
+
+  return (
+    <div className="text-4xl font-bold text-green-600 p-4">
+      ✅ Tailwind CSS v4 is working!
+    </div>
+  )
+}
+
+export default App;
